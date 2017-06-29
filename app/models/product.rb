69b9d9d8f8d349f024c47371203bd5ab624e7517
Akshar Jamgaonkar <akshar.jamgaonkar@ordercup.com>
2017-06-29 22:47:18 +0530

    added price_sum method to the Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ActiveRecord::Base
   COLORS = [["Blue","Blue"],["Red","Red"],["Yellow","Yellow"],["Black","Black"]]
   validates_presence_of :name,:description,:price
   validates_numericality_of :price
+  
+  
+  def self.price_sum(product_type)
+    self.where(:type => product_type).sum(:price)
+  end
 end
